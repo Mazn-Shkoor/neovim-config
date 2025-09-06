af01dbf251b32f10415bcc09b21c6647735cf324
Mazn-Shkoor <mazn.adnan.shkoor@gmail.com>
2025-09-06 09:47:59 +0300

    formatted lua lsp config file

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
     dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls"},
+        ensure_installed = { "lua_ls", "ts_ls" },
       })
     end,
   },
@@ -20,16 +20,16 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
-        lspconfig.ts_ls.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
 
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap=true, silent=true })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true})
 
-        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { noremap=true, silent=true })
-        vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { noremap=true, silent=true })
+      vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { noremap = true, silent = true})
+      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { noremap = true, silent = true})
     end
   }
 }
